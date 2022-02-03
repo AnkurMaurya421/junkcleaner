c5399e95f9f8ca92cfb7efd4f28832d3ee49701c
Ankur Maurya <ankurmaurya421@gmail.com>
2022-02-03 11:39:02 +0530

    added scrollbar to programs window

--- GUI.py
+++ GUI.py
@@ -10,7 +10,24 @@ def openprogramsgui():
    program_window=Toplevel(window)
    program_window.title("uninstall programs")
    app_dictionary=pg.get_applications_with_location_and_without_locations()
+   canvas = Canvas(program_window)
+   scroll_y = Scrollbar(program_window, orient="vertical", command=canvas.yview)
 
+   frame = Frame(canvas)
+   # group of widgets
+   for i in range(35):
+       Button(frame, text="hrllo").pack()
+       Label()
+   # put the frame in the canvas
+   canvas.create_window(0, 0, anchor='nw', window=frame)
+   # make sure everything is displayed before configuring the scrollregion
+   canvas.update_idletasks()
+
+   canvas.configure(scrollregion=canvas.bbox('all'),
+                    yscrollcommand=scroll_y.set)
+
+   canvas.pack(fill='both', expand=True, side='left')
+   scroll_y.pack(fill='y', side='right')
 def openfilesgui():
     files_window = Toplevel(window)
     files_window.title("Delete Files")
