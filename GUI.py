1a65feb5b10edce092393ef00c7557208b0cd7c3
Ankur Maurya <ankurmaurya421@gmail.com>
2022-02-21 11:36:20 +0530

    finished programs window

--- GUI.py
+++ GUI.py
@@ -20,32 +20,32 @@ class gui():
 
     def openprogramsgui(self):
         print("hello1")
-        '''program_window = Toplevel(self.window)
-        program_window.title("uninstall programs")
+        self.program_window = Toplevel(self.window)
+        self.program_window.title("uninstall programs")
 
-        canvas = Canvas(program_window)                                                         #adding canvas to make window scrollable
-        scroll_y = Scrollbar(program_window, orient="vertical", command=canvas.yview)
-        frame = Frame(canvas)                                                                   #adding frame inside canvas to hold buttons and widgets
+        self.canvas = Canvas(self.program_window)                                                         #adding canvas to make window scrollable
+        self.scroll_y = Scrollbar(self.program_window, orient="vertical", command=self.canvas.yview)
+        self.frame = Frame(self.canvas)                                                                   #adding frame inside canvas to hold buttons and widgets
 
 
-        Label(frame, text="Click to uninstall", anchor=CENTER, font="None 16 bold").grid(row=0, column=0, sticky=W) #adding titles
-        Label(frame, text="Last used date", font="None 16 bold ").grid(row=0, column=0, sticky=E)
+        Label(self.frame, text="Click to uninstall", anchor=CENTER, font="None 16 bold").grid(row=0, column=0, sticky=W) #adding titles
+        Label(self.frame, text="Last used date", font="None 16 bold ").grid(row=0, column=0, sticky=E)
 
 
         counter = 1
         app_dictionary = pg.get_applications_with_location()
         for i, j in app_dictionary.items():
-            Button(frame,text=i,command=lambda:pg.uninstall()).grid(row=counter, column=0, sticky=W)
-            Label(frame, text=j).grid(row=counter, column=0, sticky=E)
+            button(self,i,counter)                                      #created button object to remeber the name of program to be unistalled associated with button
+            Label(self.frame,text=j).grid(row=counter, column=0, sticky=E)
             counter += 1
                                                                                 # put the frame in the canvas
-        canvas.create_window(0, 0, anchor='nw', window=frame)
+        self.canvas.create_window(0, 0, anchor='nw', window=self.frame)
                                                                                 # make sure everything is displayed before configuring the scrollregion
-        canvas.update_idletasks()
-        canvas.configure(scrollregion=canvas.bbox('all'),
-                     yscrollcommand=scroll_y.set)
-        canvas.pack(fill='both', expand=True, side='left')
-        scroll_y.pack(fill='y', side='right')'''
+        self.canvas.update_idletasks()
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'),
+                     yscrollcommand=self.scroll_y.set)
+        self.canvas.pack(fill='both', expand=True, side='left')
+        self.scroll_y.pack(fill='y', side='right')
 
 
     def openfilesgui(self):
@@ -55,12 +55,11 @@ class gui():
 
 
 
-    '''def createbutton(self):
 
-        Button(self.window, text="Uninstall Programs", command=self.openprogramsgui).grid(row=1, column=0, sticky=E) #create 2 buttons in parent window
-        Button(self.window, text="Delete Files", command=self.openfilesgui).grid(row=2, column=0, sticky=W)'''
 
 class button:
-    def __init__(self,name):
+    counter = 1
+    app_dictionary = pg.get_applications_with_location()
+    def __init__(self,obj,name,counter):
         self.name=name
-        self.button=Button(text=self.name,command=lambda:pg.uninstall(self.name)).grid(row=counter, column=0, sticky=W)
+        self.button=Button(obj.frame,text=self.name,command=lambda:pg.uninstall(self.name)).grid(row=counter, column=0, sticky=W)
