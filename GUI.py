d6c5de0d4778455e7b95c00b41014708ee495a3c
Ankur Maurya <80671143+AnkurMaurya421@users.noreply.github.com>
2025-10-03 08:57:45 +0530

    Update GUI.py and fixed a typo

--- GUI.py
+++ GUI.py
@@ -16,7 +16,7 @@ try:
             Button(self.window, text="Delete Files", command=lambda:figui.openfilesgui(self)).grid(row=2, column=0, sticky=W)
             self.window.mainloop()         #start program
 except:
-    gui.window.distroy() #close the window in case of error
+    self.window.destroy() #close the window in case of error
 
 
 
